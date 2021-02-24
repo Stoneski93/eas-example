dccb52591e39300c5efdea97d1653d4a1a81f5d2
Norbert Kamienski <norbert.kamienski@pagepro.pl>
2021-02-24 14:34:57 +0100

    dan

--- App.tsx
+++ App.tsx
@@ -1,10 +1,57 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, Image, View, Text, TouchableOpacity } from "react-native";
+import { Audio } from "expo-av";
 
 export default function App() {
+  const [isStart, setIsStart] = useState(false);
+  const [sound, setSound] = React.useState<Audio.Sound | null>(null);
+
+  async function playSound() {
+    const { sound } = await Audio.Sound.createAsync(
+      require("./assets/pixa2.mp3")
+    );
+    setSound(sound);
+
+    await sound.playAsync();
+    await sound.setIsLoopingAsync(true);
+  }
+
+  React.useEffect(() => {
+    return sound
+      ? () => {
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
+  const onButtonPress = () => {
+    setIsStart(true);
+    playSound();
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {isStart ? (
+        <View style={{ width: "100%" }}>
+          <Image
+            style={{ width: "100%", height: "100%" }}
+            source={require("./assets/daniel.gif")}
+          />
+        </View>
+      ) : (
+        <>
+          <View style={{ flex: 1, justifyContent: "flex-end" }}>
+            <Image source={require("./assets/logo.jpeg")} />
+          </View>
+          <View style={styles.bottomContainer}>
+            <View style={styles.button}>
+              <TouchableOpacity onPress={onButtonPress}>
+                <Text style={styles.label}>Start</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </>
+      )}
     </View>
   );
 }
@@ -12,8 +59,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#002639",
     alignItems: "center",
     justifyContent: "center",
+  },
+  bottomContainer: {
+    flex: 0.6,
+    justifyContent: "flex-end",
+    paddingBottom: 100,
+  },
+  button: {
+    flexDirection: "row",
+    justifyContent: "center",
+    backgroundColor: "#f6303f",
+    padding: 10,
+    minWidth: 150,
+    borderRadius: 25,
+  },
+  label: {
+    fontSize: 20,
   },
 });
