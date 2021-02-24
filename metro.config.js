cfd8e1850a5c3bff737036e89f032081f98c7b5b
Norbert Kamienski <norbert.kamienski@pagepro.pl>
2021-02-24 18:29:20 +0100

    y

--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,5 @@
-const { getDefaultConfig } = require("metro-config");
-
-module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts },
-  } = await getDefaultConfig();
-
-  return {
-    transformer: {
-      assetPlugins: ["expo-asset/tools/hashAssetFiles"],
-    },
-    resolver: {
-      assetExts: [...assetExts, "mp3"],
-      assetExts: [...assetExts, "mp3"],
-    },
-  };
-})();
+module.exports = {
+  transformer: {
+    assetPlugins: ["expo-asset/tools/hashAssetFiles"],
+  },
+};
